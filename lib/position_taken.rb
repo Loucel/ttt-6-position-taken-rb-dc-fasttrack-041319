0885a5bad5ea41f378d46df8a6b173c44e5f2474
Loucel Torreon <loucelp@yahoo.com>
2019-04-14 16:03:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["X"," "," "," "," "," "," "," "," "]
 
 def position_taken?(board, index)
   ! board[index] == " " || board[index] == "" || board[index] == nil
