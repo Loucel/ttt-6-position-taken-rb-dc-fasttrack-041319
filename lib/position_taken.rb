9f187b6ed8e8915cb6cd14e9547ddb9f151ba050
Loucel Torreon <loucelp@yahoo.com>
2019-04-14 15:52:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+board = [" "," "," "," "," "," "," "," "," "]
+def position_taken?(board, index)
+  ! board[index] == " " || board[index] == "" || board[index] == nil
+end
+
+puts position_taken?(board, 0)
+puts position_taken?(board, 1)
+puts position_taken?(board, 2)
+puts position_taken?(board, 3)
+puts position_taken?(board, 4)
